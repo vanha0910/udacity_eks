e07f5248d1a04555924292512be3909d8ff6b104
vanha0910 <vanha.nguyenthi0910@gmail.com>
2024-09-07 06:33:28 +0000

    Update project 3

--- analytics/config.py
+++ analytics/config.py
@@ -8,13 +8,14 @@ from flask_sqlalchemy import SQLAlchemy
 # load_dotenv()
 
 db_username = os.environ["DB_USER"]
-db_password = os.environ["DB_PASSWORD"]
+db_password = "mypassword"
 db_host = os.environ.get("DB_HOST", "127.0.0.1")
 db_port = os.environ.get("DB_PORT", "5432")
 db_name = os.environ.get("DB_NAME", "postgres")
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
+logging.warning(' password for user ' + f"postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}")
 
 db = SQLAlchemy(app)
 
